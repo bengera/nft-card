3d16c0c58d882fa776af9151062fb4abfeaaa750
unknown <bengera89@gmail.com>
2021-12-26 12:57:12 +0700

    transition

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const card = document.querySelector('.card')
+
+// HEADER EVENT LISTENER
+window.addEventListener('load', () => {
+    console.log('loaded');
+    card.classList.add('show')
+    
+})
